e8631bbcdbca00e4e6b37423152a4a1197d907f5
Thiago P. Bueno <thiago.pbueno@gmail.com>
2018-05-24 18:16:13 -0300

    Add RandomPolicy (without global constraints checking)

--- tests/test_policy.py
+++ tests/test_policy.py
@@ -1,7 +1,7 @@
 from tfrddlsim.rddl import RDDL
 from tfrddlsim.parser import RDDLParser
 from tfrddlsim.compiler import Compiler
-from tfrddlsim.policy import DefaultPolicy
+from tfrddlsim.policy import DefaultPolicy, RandomPolicy
 
 import tensorflow as tf
 
@@ -27,7 +27,7 @@ class TestDefaultPolicy(unittest.TestCase):
             cls.graph2 = tf.Graph()
             cls.compiler2 = Compiler(cls.rddl2, cls.graph2)
 
-    def test_policy(self):
+    def test_default_policy(self):
         for compiler in [self.compiler1, self.compiler2]:
             with compiler.graph.as_default():
                 default = compiler.default_action_fluents
@@ -49,3 +49,49 @@ class TestDefaultPolicy(unittest.TestCase):
                 state2 = None
                 action2 = policy(state2)
                 self.assertIs(action1, action2)
+
+
+class TestRandomPolicy(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        parser = RDDLParser()
+        parser.build()
+
+        with open('rddl/Reservoir.rddl', mode='r') as file:
+            RESERVOIR = file.read()
+            cls.rddl1 = parser.parse(RESERVOIR)
+            cls.graph1 = tf.Graph()
+            cls.compiler1 = Compiler(cls.rddl1, cls.graph1, batch_mode=True)
+
+        with open('rddl/Mars_Rover.rddl', mode='r') as file:
+            MARS_ROVER = file.read()
+            cls.rddl2 = parser.parse(MARS_ROVER)
+            cls.graph2 = tf.Graph()
+            cls.compiler2 = Compiler(cls.rddl2, cls.graph2, batch_mode=True)
+
+    def test_random_policy(self):
+        batch_size = 1000
+
+        for compiler in [self.compiler1, self.compiler2]:
+
+            with compiler.graph.as_default():
+
+                policy = RandomPolicy(compiler, batch_size)
+
+                state = compiler.compile_initial_state(batch_size)
+                action = policy(state)
+
+                action_size = compiler.action_size
+                action_dtype = compiler.action_dtype
+                action_default_fluents = compiler.compile_default_action(batch_size)
+
+                self.assertIsInstance(action, tuple)
+                self.assertEqual(len(action), len(action_size))
+                self.assertEqual(len(action), len(action_dtype))
+                for fluent, size, dtype, default in zip(action, action_size, action_dtype, action_default_fluents):
+                    self.assertIsInstance(fluent, tf.Tensor)
+                    self.assertListEqual(fluent.shape.as_list(), [batch_size] + list(size))
+                    self.assertEqual(fluent.dtype, dtype)
+                    self.assertEqual(fluent.shape, default.shape)
+                    self.assertEqual(fluent.dtype, default.dtype)
--- tfrddlsim/policy.py
+++ tfrddlsim/policy.py
@@ -16,3 +16,54 @@ class DefaultPolicy(Policy):
 
     def __call__(self, state, timestep=None):
         return self._default
+
+
+class RandomPolicy(Policy):
+
+    MAX_REAL_VALUE = 5.0
+    MAX_INT_VALUE = 5
+
+    def __init__(self, compiler, batch_size):
+        self.compiler = compiler
+        self.batch_size = batch_size
+
+    def __call__(self, state, timestep=None):
+        action = []
+
+        action_fluent_ordering = self.compiler.action_fluent_ordering
+        action_dtype = self.compiler.action_dtype
+        action_size = self.compiler.action_size
+
+        action_bound_constraints = self.compiler.compile_action_bound_constraints(state)
+
+        for name, dtype, size in zip(action_fluent_ordering, action_dtype, action_size):
+            shape = [self.batch_size] + list(size)
+
+            if dtype == tf.float32:
+                bounds = action_bound_constraints.get(name)
+                if bounds is None:
+                    low, high = -self.MAX_REAL_VALUE, self.MAX_REAL_VALUE
+                    dist = tf.distributions.Uniform(low=low, high=high)
+                    a = dist.sample(shape)
+                else:
+                    low, high = bounds
+                    batch = (low is not None and low.batch) or (high is not None and high.batch)
+                    low = low.tensor if low is not None else -self.MAX_REAL_VALUE
+                    high = high.tensor if high is not None else self.MAX_REAL_VALUE
+                    dist = tf.distributions.Uniform(low=low, high=high)
+                    if batch:
+                        a = dist.sample()
+                    else:
+                        a = dist.sample(shape)
+            elif dtype == tf.int32:
+                logits = [1.0] * self.MAX_INT_VALUE
+                dist = tf.distributions.Categorical(logits=logits, dtype=tf.int32)
+                a = dist.sample(shape)
+            elif dtype == tf.bool:
+                probs = 0.5
+                dist = tf.distributions.Bernoulli(probs=probs, dtype=tf.bool)
+                a = dist.sample(shape)
+
+            action.append(a)
+
+        return tuple(action)
