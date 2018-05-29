34e8f382d268044eeee79073e37ebd768a159502
Thiago P. Bueno <thiago.pbueno@gmail.com>
2018-05-29 18:13:52 -0300

    Display round init information

--- tfrddlsim/visualizer.py
+++ tfrddlsim/visualizer.py
@@ -19,29 +19,34 @@ class BasicVisualizer(Visualizer):
         super().__init__(compiler, verbose)
 
     def render(self, trajectories, batch=None):
-        states, actions, rewards = trajectories
+        non_fluents, states, actions, interms, rewards = trajectories
         if batch is None:
             states = [(s[0], s[1][0]) for s in states]
+            interms = [(f[0], f[1][0]) for f in interms]
             actions = [(a[0], a[1][0]) for a in actions]
             rewards = rewards[0]
-            self._render_batch(states, actions, rewards)
+            self._render_batch(non_fluents, states, actions, interms, rewards)
 
-    def _render_batch(self, states, actions, rewards, horizon=None):
+    def _render_batch(self, non_fluents, states, actions, interms, rewards, horizon=None):
         if horizon is None:
             horizon = len(states[0][1])
+            self._render_round_init(horizon, non_fluents)
             for t in range(horizon):
                 s = [(s[0], s[1][t]) for s in states]
+                f = [(f[0], f[1][t]) for f in interms]
                 a = [(a[0], a[1][t]) for a in actions]
                 r = rewards[t]
-                self._render_timestep(t, s, a, r)
+                self._render_timestep(t, s, a, f, r)
             self._render_round_end(rewards)
 
-    def _render_timestep(self, t, s, a, r):
+    def _render_timestep(self, t, s, a, f, r):
         print("============================")
         print("TIME = {}".format(t))
         print("============================")
         fluent_variables = self.compiler.action_fluent_variables
         self._render_fluent_timestep('action', a, fluent_variables)
+        fluent_variables = self.compiler.interm_fluent_variables
+        self._render_fluent_timestep('interms', f, fluent_variables)
         fluent_variables = self.compiler.state_fluent_variables
         self._render_fluent_timestep('states', s, fluent_variables)
         self._render_reward(r)
@@ -51,6 +56,7 @@ class BasicVisualizer(Visualizer):
             name, fluent = fluent
             _, variables = fluent_variables
             print(name)
+            fluent = fluent.flatten()
             for variable, value in zip(variables, fluent):
                 print('- {}: {} = {}'.format(fluent_type, variable, value))
         print()
@@ -58,6 +64,13 @@ class BasicVisualizer(Visualizer):
     def _render_reward(self, r):
         print("reward = {:.4f}".format(float(r)))
         print()
+
+    def _render_round_init(self, horizon, non_fluents):
+        print('*********************************************************')
+        print('>>> ROUND INIT, horizon = {}'.format(horizon))
+        print('*********************************************************')
+        fluent_variables = self.compiler.non_fluent_variables
+        self._render_fluent_timestep('non-fluents', non_fluents, fluent_variables)
 
     def _render_round_end(self, rewards):
         print("*********************************************************")
